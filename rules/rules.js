05501af91695583473ee2c26e4aea4587b69e63e
sweplaysYT <rasmus.svensk47@gmail.com>
2022-01-17 12:15:04 +0100

    Desktop Version of the site is now done

---
+++ rules/rules.js
@@ -0,0 +1,50 @@
+function homeLinkHover() {
+    const homeBar = document.getElementById('navbar-home-bar');
+    setTimeout(function() {
+        homeBar.style.backgroundColor = "rgb(0, 195, 255)";
+        homeBar.style.width = "128px"
+        homeBar.style.transition = "width 0.3s";
+    }, 1)
+}
+
+function homeLinkStopHover() {
+    const homeBar = document.getElementById('navbar-home-bar');
+    setTimeout(function() {
+        homeBar.style.width = "0px"
+        homeBar.style.transition = "width 0.3s";
+    }, 1)
+}
+
+function faqLinkHover() {
+    const faqBar = document.getElementById('navbar-faq-bar');
+    setTimeout(function() {
+        faqBar.style.backgroundColor = "rgb(0, 195, 255)";
+        faqBar.style.width = "128px"
+        faqBar.style.transition = "width 0.3s";
+    }, 1)
+}
+
+function faqLinkStopHover() {
+    const faqBar = document.getElementById('navbar-faq-bar');
+    setTimeout(function() {
+        faqBar.style.width = "0px"
+        faqBar.style.transition = "width 0.3s";
+    }, 1)
+}
+
+function rulesLinkHover() {
+    const rulesBar = document.getElementById('navbar-rules-bar');
+    setTimeout(function() {
+        rulesBar.style.backgroundColor = "rgb(0, 195, 255)";
+        rulesBar.style.width = "128px"
+        rulesBar.style.transition = "width 0.3s";
+    }, 1)
+}
+
+function rulesLinkStopHover() {
+    const rulesBar = document.getElementById('navbar-rules-bar');
+    setTimeout(function() {
+        rulesBar.style.width = "0px"
+        rulesBar.style.transition = "width 0.3s";
+    }, 1)
+}
